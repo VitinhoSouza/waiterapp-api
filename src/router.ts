6fc10574411b223a32c6f69da02d6df2328595e6
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2025-01-07 10:46:43 -0300

    feat: create and list categories + create and list products

--- src/router.ts
+++ src/router.ts
@@ -1,22 +1,37 @@
+import path from 'node:path';
+
 import { Router } from 'express';
+import multer from 'multer';
+
 import { listCategories } from './app/useCases/categories/listCategories';
+import { createCategory } from './app/useCases/categories/createCategory';
+import { listProducts } from './app/useCases/products/listProducts';
+import { createProduct } from './app/useCases/products/createProduct';
 
 export const router = Router();
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, callback) {
+      callback(null, path.resolve(__dirname, '..', 'uploads'));
+    },
+    filename(req, file, callback) {
+      callback(null, `${Date.now()}-${file.originalname}`)
+    },
+  }),
+});
 
 //List Categories
 router.get('/categories', listCategories);
 
 //Create Category
-router.post('/categories', (req, res) => {
-});
+router.post('/categories', createCategory);
 
 //List Products
-router.get('/products', (req, res) => {
-});
+router.get('/products', listProducts);
 
 //Create Product
-router.post('/products', (req, res) => {
-});
+router.post('/products', upload.single('image'), createProduct);
 
 //Get Products by category
 router.get('/categories/:categoryId/products', (req, res) => {
