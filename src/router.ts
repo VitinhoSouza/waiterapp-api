e1f5fd8ea1e373662520234c58365d2284836c29
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2025-01-07 10:58:34 -0300

    feat: list products by category

--- src/router.ts
+++ src/router.ts
@@ -7,6 +7,7 @@ import { listCategories } from './app/useCases/categories/listCategories';
 import { createCategory } from './app/useCases/categories/createCategory';
 import { listProducts } from './app/useCases/products/listProducts';
 import { createProduct } from './app/useCases/products/createProduct';
+import { listProductsByCategory } from './app/useCases/categories/listProductsByCategory';
 
 export const router = Router();
 
@@ -34,8 +35,7 @@ router.get('/products', listProducts);
 router.post('/products', upload.single('image'), createProduct);
 
 //Get Products by category
-router.get('/categories/:categoryId/products', (req, res) => {
-});
+router.get('/categories/:categoryId/products', listProductsByCategory);
 
 //List Orders
 router.get('/orders', (req, res) => {
